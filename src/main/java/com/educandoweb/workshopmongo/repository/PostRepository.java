008ceea6ee59b97d83dc5f26e5e42b8ecc78f51e
DiAlves <das@ecomp.poli.br>
2020-03-27 12:12:38 -0300

    Consulta simples com @Query

--- src/main/java/com/educandoweb/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/educandoweb/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.educandoweb.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.educandoweb.workshopmongo.domain.Post;
@@ -10,6 +11,10 @@ import com.educandoweb.workshopmongo.domain.Post;
 //Spring Data
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+	
+	//Consulta atraves de Json
+	@Query("{ title: { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
