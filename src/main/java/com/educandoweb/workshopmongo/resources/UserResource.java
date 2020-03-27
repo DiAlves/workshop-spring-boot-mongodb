71b107f625835270401c941b3254e5442b15b88a
DiAlves <das@ecomp.poli.br>
2020-03-27 10:08:39 -0300

    Delecao de usuario com DELETE

--- src/main/java/com/educandoweb/workshopmongo/resources/UserResource.java
+++ src/main/java/com/educandoweb/workshopmongo/resources/UserResource.java
@@ -55,4 +55,11 @@ public class UserResource {
 		return ResponseEntity.created(uri).build();
 	}
 	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	// Path Variable -> pegar a variavel do url
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
 }
